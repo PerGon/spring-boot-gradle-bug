f7d4c6fe92c47d57e248fee397a336586c0ef108
pedro_goncalves <pergon@gmail.com>
2014-01-10 12:54:50 +0100

    project to reproduce a bug

--- src/main/java/com/sysmon/Application.java
+++ src/main/java/com/sysmon/Application.java
@@ -16,6 +16,7 @@ public class Application {
 	public static void main(String[] args) {
 		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 		root.setLevel(Level.INFO);
+
 		SpringApplication.run(ApplicationBootstrap.class, args);
 	}
 }
